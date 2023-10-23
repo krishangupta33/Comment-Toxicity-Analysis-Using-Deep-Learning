2885137a312b879ce1800f89b1d8cd63c3bde9ba
Om Sangwan <omsangwan16@gmail.com>
2023-10-23 00:02:53 -0400

    Created Streamlit App

---
+++ streamlit.py
@@ -0,0 +1,19 @@
+import streamlit as st
+import tensorflow as tf
+
+#Reading the model
+model = tf.keras.models.load_model('toxicity.h5')
+
+def main():
+    st.title("Toxicity Detection")
+    st.subheader("Enter the text to check for toxicity")
+    text = st.text_input("Enter the text here")
+    if st.button("Predict"):
+        prediction = model.predict([text])
+        if prediction[0][0] > 0.5:
+            st.write("Toxic")
+        else:
+            st.write("Not Toxic")
+
+if __name__ == '__main__':
+    main()
